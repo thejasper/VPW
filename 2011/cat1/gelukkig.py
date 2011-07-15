27939d216293f665ccfeb751e6fbf582f23ade83
Jasper Desmadryl <jasper_desmadryl@hotmail.com>
2011-07-15 16:09:26 +0200

    First commit of "Gelukkige getallen"

---
+++ 2011/cat1/gelukkig.py
@@ -0,0 +1,28 @@
+#!usr/bin/python
+import sys
+
+def line():
+    return sys.stdin.readline().strip()
+
+def digits_sq(n):
+    while n:
+        n, dig = divmod(n, 10)
+        yield dig**2
+
+def solve(n):
+    seen = set([n])
+    while 1:
+        n = sum(digits_sq(n))
+        if n == 1:
+            return 'JA'
+        elif n in seen:
+            return 'NEE'
+        seen.add(n)
+
+def main():
+    num = int(line())
+    for _ in range(num):
+        print solve(int(line()))
+
+if __name__ == '__main__':
+    main()
